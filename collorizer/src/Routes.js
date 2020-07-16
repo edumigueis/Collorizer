9eae07ee27c29119387de75dd0c409f760091369
Eduardo Migueis <57845961+edumigueis@users.noreply.github.com>
2020-07-16 18:04:45 -0300

    Init Color Conversor

--- collorizer/src/Routes.js
+++ collorizer/src/Routes.js
@@ -3,11 +3,13 @@ import NotFound from './components/notFound/NotFound.vue';
 import ColorLab from './components/colorLab/ColorLab.vue';
 import Palettes from './components/paletteCreator/PaletteCreator.vue';
 import ImagePalette from './components/imagePalette/ImagePalette.vue';
+import ColorConvert from './components/colorConversor/ColorConversor.vue';
 import Main from './components/main/Main.vue';
 
 export const routes = [
     { path: '/', component: Welcome, titulo: 'Eduardo Migueis'},
     { path: '/lab', component: ColorLab, titulo: 'Color Lab'},
+    { path: '/lab/convert', component: ColorConvert, titulo: 'Color Conversor'},
     { path: '/create/palette', component: Palettes, titulo: 'Create Pallettes'},
     { path: '/image/palette', component: ImagePalette, titulo: 'Image Palette'},
     { path: '/create', component: Main, titulo: 'Create'},
